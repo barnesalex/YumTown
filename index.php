87afcd9dd10de6d12d836be8103bbee422ab5bcf
root <root@ip-172-31-29-248.us-west-2.compute.internal>
2017-12-10 19:04:33 +0000

    Fixed padding issue with Widget

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <!--        <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, width=device-width" />-->
         <meta name="description" content="SE Project">
-        <meta name="author"  content="Stuti, John Andrew, Alexander">
+        <meta name="author"  content="Stuti, John, Andrew, Alexander">
         
         <title>YumTown</title>
         
